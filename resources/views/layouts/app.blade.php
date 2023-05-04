737a2080bb11ae8606c639e60d0693fef6ce67d3
Gabriel <dev.gabriel1996@gmail.com>
2023-05-04 14:12:53 -0300

    fix: :hammer: correções no layouts/app
    corrigido os links de acesso ao bootstrap, agora é utilizado através de cdn

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -10,21 +11,20 @@
     <title>{{ config('app.name', 'MyFitness') }}</title>
 
     <!-- CSS Animated -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
 
-    <!-- CSS Bootstrap -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!--Bootstrap-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
-    <!-- Scripts -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    <!--Bootstrap-->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 </head>
+
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-primary shadow-sm">
@@ -46,128 +46,127 @@
                     <ul class="navbar-nav ms-auto">
                         <!-- Authentication Links -->
                         @guest
-                            @if (Route::has('login'))
-                                
-                                <li class="nav-item dropleft">
-                                    <a class="nav-link dropdown-toggle text-light" href="#" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                        {{ __('messages.Login') }}
-                                    </a>
-                                    <div class="dropdown-menu p-4" style="width:300px; margin-left:60vw;">
-                                        <form method="POST" action="{{ route('login') }}">
-                                            @csrf
-                                            
-                                            <div class="row mb-1">
-                                                <label for="password" class="col-md-4">{{ __('E-mail') }}</label>
-                                            </div>
+                        @if (Route::has('login'))
 
-                                            <div class="row mb-3">
-                                                <div class="col-md-12">
-                                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                        <li class="nav-item dropleft">
+                            <a class="nav-link dropdown-toggle text-light" href="#" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                {{ __('messages.Login') }}
+                            </a>
+                            <div class="dropdown-menu p-4" style="width:300px; margin-left:60vw;">
+                                <form method="POST" action="{{ route('login') }}">
+                                    @csrf
 
-                                                    @error('email')
-                                                        <span class="invalid-feedback" role="alert">
-                                                            <strong>{{ $message }}</strong>
-                                                        </span>
-                                                    @enderror
-                                                </div>
-                                            </div>
-
-                                            <div class="row mb-1">
-                                                <label for="password" class="col-md-4">{{ __('messages.Password') }}</label>
-                                            </div>
-
-                                            <div class="row mb-3">
-                                                <div class="col-md-12">
-                                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                                    @error('password')
-                                                        <span class="invalid-feedback" role="alert">
-                                                            <strong>{{ $message }}</strong>
-                                                        </span>
-                                                    @enderror
-                                                </div>
-                                            </div>
-
-                                            <div class="row mb-3">
-                                                <div class="col-md-12">
-                                                    <div class="form-check">
-                                                        <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                                        <label class="form-check-label" for="remember">
-                                                            {{ __('messages.Remember') }}
-                                                        </label>
-                                                    </div>
-                                                </div>
-                                            </div>
-
-                                            <div class="row mb-0">
-                                                <div class="col-md-12">
-                                                    <button type="submit" class="btn btn-primary col-sm-12">
-                                                        {{ __('messages.Login') }}
-                                                    </button>
-
-                                                    @if (Route::has('password.request'))
-                                                        <a class="btn btn-link" href="{{ route('password.request') }}">
-                                                            {{ __('messages.ForgotMyPassword') }}
-                                                        </a>
-                                                    @endif
-                                                </div>
-                                            </div>
-                                        </form>
+                                    <div class="row mb-1">
+                                        <label for="password" class="col-md-4">{{ __('E-mail') }}</label>
                                     </div>
-                                </li>
-                            @endif
 
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link text-light" href="{{ route('register') }}">{{ __('messages.Register') }}</a>
-                                </li>
-                            @endif
+                                    <div class="row mb-3">
+                                        <div class="col-md-12">
+                                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                                            @error('email')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                            @enderror
+                                        </div>
+                                    </div>
+
+                                    <div class="row mb-1">
+                                        <label for="password" class="col-md-4">{{ __('messages.Password') }}</label>
+                                    </div>
+
+                                    <div class="row mb-3">
+                                        <div class="col-md-12">
+                                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+
+                                            @error('password')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                            @enderror
+                                        </div>
+                                    </div>
+
+                                    <div class="row mb-3">
+                                        <div class="col-md-12">
+                                            <div class="form-check">
+                                                <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                                <label class="form-check-label" for="remember">
+                                                    {{ __('messages.Remember') }}
+                                                </label>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="row mb-0">
+                                        <div class="col-md-12">
+                                            <button type="submit" class="btn btn-primary col-sm-12">
+                                                {{ __('messages.Login') }}
+                                            </button>
+
+                                            @if (Route::has('password.request'))
+                                            <a class="btn btn-link" href="{{ route('password.request') }}">
+                                                {{ __('messages.ForgotMyPassword') }}
+                                            </a>
+                                            @endif
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </li>
+                        @endif
+
+                        @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link text-light" href="{{ route('register') }}">{{ __('messages.Register') }}</a>
+                        </li>
+                        @endif
                         @else
 
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle text-light" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ __('messages.HelloMessage') }}
-                                    {{ Auth::user()->name }}
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle text-light" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ __('messages.HelloMessage') }}
+                                {{ Auth::user()->name }}
+                            </a>
+                            <div class="dropdown-menu">
+                                <a class="dropdown-item" href="{{ route('perfil') }}">
+                                    {{ __('messages.Profile') }}
                                 </a>
-                                <div class="dropdown-menu">
-                                    <a class="dropdown-item" href="{{ route('perfil') }}">
-                                        {{ __('messages.Profile') }}
-                                    </a>
-                                    <a class="dropdown-item" href="{{ route('createFood') }}">
-                                        {{ __('messages.AddFood') }}
-                                    </a>
-                                    <a class="dropdown-item" href="{{ route('allFoodsView') }}">
-                                        {{ __('messages.MyFoods') }}
-                                    </a>
-                                </div>
-                            </li>
-
-                            <!-- Separador -->
-
-                            <li class="nav-item">
-                                <a class="nav-link text-light">
-                                    |
+                                <a class="dropdown-item" href="{{ route('createFood') }}">
+                                    {{ __('messages.AddFood') }}
                                 </a>
-                            </li>
-
-                            <li class="nav-item">
-                                <a class="nav-link text-light" href="{{ route('goalView') }}" role="button">
-                                    {{ __('messages.MyGoals') }}
+                                <a class="dropdown-item" href="{{ route('allFoodsView') }}">
+                                    {{ __('messages.MyFoods') }}
                                 </a>
-                            </li>
+                            </div>
+                        </li>
 
-                            <li class="nav-item">
-                                <a class="nav-link text-light" href="{{ route('logout') }}"
-                                    onclick="event.preventDefault();
+                        <!-- Separador -->
+
+                        <li class="nav-item">
+                            <a class="nav-link text-light">
+                                |
+                            </a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link text-light" href="{{ route('goalView') }}" role="button">
+                                {{ __('messages.MyGoals') }}
+                            </a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link text-light" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                     document.getElementById('logout-form').submit();">
-                                    {{ __('messages.SignOut') }}
-                                </a>
+                                {{ __('messages.SignOut') }}
+                            </a>
 
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
-                            </li>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
+                        </li>
                         @endguest
                     </ul>
                 </div>
@@ -178,24 +177,27 @@
             @yield('content')
         </main>
 
-        
+
 
     </div>
 
     <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
         <div class="col-md-4 d-flex align-items-center">
-        <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
-            <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"></use></svg>
-        </a>
-        <span class="text-muted">© 2023 MyFitness, Inc</span>
+            <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+                <svg class="bi" width="30" height="24">
+                    <use xlink:href="#bootstrap"></use>
+                </svg>
+            </a>
+            <span class="text-muted">© 2023 MyFitness, Inc</span>
         </div>
 
         <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
-        <li class="ms-3"><a class="text-muted" href="#"><img src="{{ asset('img/icons/twitter.png') }}" height="32" width="32"></a></li>
-        <li class="ms-3"><a class="text-muted" href="#"><img src="{{ asset('img/icons/instagram.png') }}" height="32" width="32"></a></li>
-        <li class="ms-3"><a class="text-muted" href="#"><img src="{{ asset('img/icons/facebook.png') }}" height="32" width="32"></a></li>
+            <li class="ms-3"><a class="text-muted" href="#"><img src="{{ asset('img/icons/twitter.png') }}" height="32" width="32"></a></li>
+            <li class="ms-3"><a class="text-muted" href="#"><img src="{{ asset('img/icons/instagram.png') }}" height="32" width="32"></a></li>
+            <li class="ms-3"><a class="text-muted" href="#"><img src="{{ asset('img/icons/facebook.png') }}" height="32" width="32"></a></li>
         </ul>
     </footer>
 
 </body>
+
 </html>
