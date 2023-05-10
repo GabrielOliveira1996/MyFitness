013b0da96317c50ab07fd8ec2d6143bc7eaa42fc
Gabriel <dev.gabriel1996@gmail.com>
2023-05-10 11:54:45 -0300

    feat: :art: adicionado timezone, repositório de metas e método findUserGoals
    timezone adicionado a construtor para aplicar a todos os métodos o mesmo timezone, repositório de metas (Goal) chamado em novo método findUserGoals, método utilizado para pegar alimentos por tipos e data, nesse método também são feito calculos para cada tipo de refeição e para cada macro nutriente, por exemplo, em $breakfast, $lunch, $snack, $dinner, $preWorkout e $postWorkout são capturados as refeições através de sua data de criação, e nas variáveis $caloriesOfTheDay, $carbohydrateOfTheDay, $proteinOfTheDay e $totalFatOfTheDay são feito os calculos das calorias e cada macro nutriente (carboidratos, proteínas e gorduras) por essas refeições.

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -5,16 +5,21 @@ namespace App\Services;
 use App\Validator\UserValidator;
 use App\Repository\User\IUserRepository;
 use Illuminate\Support\Facades\Auth;
+use App\Repository\Goal\IGoalRepository;
 
 class UserService
 {
     private $_userValidator;
     private $_userRepository;
+    private $_goalRepository;
+    private $_timezone;
 
-    public function __construct(UserValidator $userValidator, IUserRepository $userRepository)
+    public function __construct(UserValidator $userValidator, IUserRepository $userRepository, IGoalRepository $goalRepository)
     {
         $this->_userValidator = $userValidator;
         $this->_userRepository = $userRepository;
+        $this->_goalRepository = $goalRepository;
+        $this->_timezone = date_default_timezone_set('America/Sao_Paulo');
     }
 
     public function register($user)
@@ -28,5 +33,83 @@ class UserService
         $id = Auth::user()->id;
         $update = $this->_userRepository->update($user, $id);
         return $update;
+    }
+
+    public function findUserGoals($date)
+    {
+        $id = Auth::user()->id;
+        $user = $this->_userRepository->find($id);
+
+        //Tipos de refeição.
+        $breakfast = $this->_goalRepository->breakfast($date);
+        $lunch = $this->_goalRepository->lunch($date);
+        $snack = $this->_goalRepository->snack($date);
+        $dinner = $this->_goalRepository->dinner($date);
+        $preWorkout = $this->_goalRepository->preWorkout($date);
+        $postWorkout = $this->_goalRepository->postWorkout($date);
+
+        //Metas estabelecidas pelo usuário.
+        if ($user) {
+            $goalCalories = $user['daily_calories'];
+            $goalCarbohydrate = $user['daily_carbohydrate'];
+            $goalProtein = $user['daily_protein'];
+            $goalTotalFat = $user['daily_fat'];
+        }
+
+        //Resultados do dia.
+        $caloriesOfTheDay = array_sum([
+            $breakfast->sum('calories'),
+            $lunch->sum('calories'),
+            $snack->sum('calories'),
+            $dinner->sum('calories'),
+            $preWorkout->sum('calories'),
+            $postWorkout->sum('calories')
+        ]);
+
+        $carbohydratesOfTheDay = array_sum([
+            $breakfast->sum('carbohydrate'),
+            $lunch->sum('carbohydrate'),
+            $snack->sum('carbohydrate'),
+            $dinner->sum('carbohydrate'),
+            $preWorkout->sum('carbohydrate'),
+            $postWorkout->sum('carbohydrate')
+        ]);
+
+        $proteinOfTheDay = array_sum([
+            $breakfast->sum('protein'),
+            $lunch->sum('protein'),
+            $snack->sum('protein'),
+            $dinner->sum('protein'),
+            $preWorkout->sum('protein'),
+            $postWorkout->sum('protein')
+        ]);
+
+        $totalFatOfTheDay = array_sum([
+            $breakfast->sum('total_fat'),
+            $lunch->sum('total_fat'),
+            $snack->sum('total_fat'),
+            $dinner->sum('total_fat'),
+            $preWorkout->sum('total_fat'),
+            $postWorkout->sum('total_fat')
+        ]);
+
+        return [
+            'date' => $date,
+            'user' => $user,
+            'breakfast' => $breakfast,
+            'lunch' => $lunch,
+            'snack' => $snack,
+            'dinner' => $dinner,
+            'preWorkout' => $preWorkout,
+            'postWorkout' => $postWorkout,
+            'goalCalories' => $goalCalories,
+            'goalCarbohydrate' => $goalCarbohydrate,
+            'goalProtein' => $goalProtein,
+            'goalTotalFat' => $goalTotalFat,
+            'caloriesOfTheDay' => $caloriesOfTheDay,
+            'carbohydratesOfTheDay' => $carbohydratesOfTheDay,
+            'proteinOfTheDay' => $proteinOfTheDay,
+            'totalFatOfTheDay' => $totalFatOfTheDay
+        ];
     }
 }
