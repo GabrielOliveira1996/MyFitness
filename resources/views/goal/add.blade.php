3502fedcc114b58797c36f647068d2943633169d
Gabriel <dev.gabriel1996@gmail.com>
2023-05-11 11:48:54 -0300

    fix: :hammer: rota searchFood removido de formulário

--- resources/views/goal/add.blade.php
+++ resources/views/goal/add.blade.php
@@ -8,7 +8,7 @@
 
         <p class="text-center">{{ __('messages.SearchDescription') }}</p>
 
-        <form method="post" action="{{ route('searchFood', ['type' => $_GET['type']]) }}">
+        <form method="post">
             @csrf
             <div class="row d-flex justify-content-center">
                 <div class="col-md-5">
@@ -35,31 +35,28 @@
                     <th scope="col">{{ __('messages.Fat') }}</th>
                     <th scope="col">{{ __('messages.SaturatedFat') }}</th>
                     <th scope="col">{{ __('messages.TransFat') }}</th>
-                    <th scope="col"></th>
                     <th scope="col">{{ __('messages.Add') }}</th>
                 </tr>
             </thead>
             <tbody>
                 @if(!empty($foods))
-                    @foreach($foods as $key => $value)
-                    <tr>
-                        <form method="POST" action="{{ route('addFoodToDayGoal') }}">
-                            @csrf
-                            <td><input type="text" class="form-control" name="name" value="{{__($value->name)}}" step="any" readonly></th>
-                            <td><input id="quantityGramsId-{{__($key)}}" type="number" class="form-control" name="quantity_grams" value="{{__($value->quantity_grams)}}" step="any"></td>
-                            <td><input id="quantityCalorieId-{{__($key)}}" type="number" class="form-control border-0" name="calories" value="{{__($value->calories)}}" step="any" readonly></td>
-                            <td><input id="quantityCarbohydrateId-{{__($key)}}" type="number" class="form-control border-0" name="carbohydrate" value="{{__($value->carbohydrate)}}" step="any" readonly></td>
-                            <td><input id="quantityProteinId-{{__($key)}}" type="number" class="form-control border-0" name="protein" value="{{__($value->protein)}}" step="any" readonly></td>
-                            <td><input id="quantityTotalFatId-{{__($key)}}" type="number" class="form-control border-0" name="total_fat" value="{{__($value->total_fat)}}" step="any" readonly></td>
-                            <td><input id="quantitySaturatedFatId-{{__($key)}}" type="number" class="form-control border-0" name="saturated_fat" value="{{__($value->saturated_fat)}}" step="any" readonly></td>
-                            <td><input id="quantityTransFatId-{{__($key)}}" type="number" class="form-control border-0" name="trans_fat" value="{{__($value->trans_fat)}}" step="any" readonly></td>
-                            <td>
-                                <input type="hidden" class="form-control" name="type_of_meal" value="{{ $_GET['type'] }}" readonly>
-                            </td>
-                            <td><button class="btn btn-primary">{{ __('messages.Add') }}</button></td>
-                        </form>
-                    </tr>
-                    @endforeach
+                @foreach($foods as $key => $value)
+                <tr>
+                    <form method="POST" action="{{ route('goal.addfood', ['type' => $type]) }}">
+                        @csrf
+                        <td><input type="text" class="form-control" name="name" value="{{__($value->name)}}" step="any" readonly></th>
+                        <td><input id="quantityGramsId-{{__($key)}}" type="number" class="form-control" name="quantity_grams" value="{{__($value->quantity_grams)}}" step="any"></td>
+                        <td><input id="quantityCalorieId-{{__($key)}}" type="number" class="form-control border-0" name="calories" value="{{__($value->calories)}}" step="any" readonly></td>
+                        <td><input id="quantityCarbohydrateId-{{__($key)}}" type="number" class="form-control border-0" name="carbohydrate" value="{{__($value->carbohydrate)}}" step="any" readonly></td>
+                        <td><input id="quantityProteinId-{{__($key)}}" type="number" class="form-control border-0" name="protein" value="{{__($value->protein)}}" step="any" readonly></td>
+                        <td><input id="quantityTotalFatId-{{__($key)}}" type="number" class="form-control border-0" name="total_fat" value="{{__($value->total_fat)}}" step="any" readonly></td>
+                        <td><input id="quantitySaturatedFatId-{{__($key)}}" type="number" class="form-control border-0" name="saturated_fat" value="{{__($value->saturated_fat)}}" step="any" readonly></td>
+                        <td><input id="quantityTransFatId-{{__($key)}}" type="number" class="form-control border-0" name="trans_fat" value="{{__($value->trans_fat)}}" step="any" readonly></td>
+
+                        <td><button class="btn btn-primary">{{ __('messages.Add') }}</button></td>
+                    </form>
+                </tr>
+                @endforeach
                 @endif
             </tbody>
         </table>
@@ -68,7 +65,7 @@
 
     <div class="d-flex justify-content-center mt-5">
         @if(!empty($foods))
-            {{$foods->links()}}
+        {{$foods->links()}}
         @endif
     </div>
 
