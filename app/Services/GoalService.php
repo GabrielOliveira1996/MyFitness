ad5ddbe5dfca880bfcac4025dc6ca6ac786a2c2c
Gabriel <dev.gabriel1996@gmail.com>
2023-05-16 13:03:01 -0300

    fix: :hammer: modificações em GoalService
    classe GoalValidator agora é chamada para validar create e update de alimento, método de exclusão removido, agora ele é chamado em controller

--- app/Services/GoalService.php
+++ app/Services/GoalService.php
@@ -4,41 +4,34 @@ namespace App\Services;
 
 use App\Repository\Goal\IGoalRepository;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Validator;
+use App\Validator\GoalValidator;
 
 class GoalService
 {
     private $_goalRepository;
+    private $_goalValidator;
     private $_timezone;
 
-    public function __construct(IGoalRepository $goalRepository)
+    public function __construct(IGoalRepository $goalRepository, GoalValidator $goalValidator)
     {
         $this->_goalRepository = $goalRepository;
+        $this->_goalValidator = $goalValidator;
         $this->_timezone = date_default_timezone_set('America/Sao_Paulo');
     }
 
     public function create($type, $goal)
     {
+        $goal += ['type_of_meal' => $type];
         $user = Auth::user();
+        $this->_goalValidator->food($goal);
         $create = $this->_goalRepository->create($type, $goal, $user);
         return $create;
     }
 
-    public function find($id)
-    {
-        $find = $this->_goalRepository->find($id);
-        return $find;
-    }
-
     public function update($id, $goal)
     {
+        $this->_goalValidator->food($goal);
         $update = $this->_goalRepository->update($id, $goal);
         return $update;
-    }
-
-    public function delete($id)
-    {
-        $delete = $this->_goalRepository->delete($id);
-        return $delete;
     }
 }
