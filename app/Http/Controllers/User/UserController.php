d5a1aee85db7917098a59b7f75c1da71b78a6ed7
Gabriel <dev.gabriel1996@gmail.com>
2023-05-16 11:24:44 -0300

    refactor: :hammer: modificações em UserController
    removido chamada de classe Request e UserService, adicionado método index, método retorna página home ao realizar login

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -2,21 +2,20 @@
 
 namespace App\Http\Controllers\User;
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
-use App\Services\UserService;
 
 class UserController extends Controller
 {
-    private $_request;
-    private $_userService;
 
-    public function __construct(Request $request, UserService $userService)
+    public function __construct()
     {
         $this->middleware('auth');
-        $this->_request = $request;
-        $this->_userService = $userService;
+    }
+
+    public function index()
+    {
+        return view('home');
     }
 
     public function profile()
