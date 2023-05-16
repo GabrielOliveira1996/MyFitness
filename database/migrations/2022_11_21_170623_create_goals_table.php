f236d0f04ead97bf94fee58f3fb43fcfe7e5acd6
Gabriel <dev.gabriel1996@gmail.com>
2023-05-16 12:50:30 -0300

    fix: :hammer: atributo date retirado de goal migration

--- database/migrations/2022_11_21_170623_create_goals_table.php
+++ database/migrations/2022_11_21_170623_create_goals_table.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    
+
     public function up()
     {
         Schema::create('goals', function (Blueprint $table) {
@@ -23,7 +23,6 @@ return new class extends Migration
             $table->decimal('saturated_fat');
             $table->decimal('trans_fat');
             $table->string('type_of_meal');
-            $table->string('date');
             $table->timestamps();
         });
     }
