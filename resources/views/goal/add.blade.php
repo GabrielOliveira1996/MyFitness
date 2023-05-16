ab2714de4590f5350ab9e8fdc8a8c3721d145d27
Gabriel <dev.gabriel1996@gmail.com>
2023-05-16 12:49:55 -0300

    feat: adicionado listagem de erros de validação

--- resources/views/goal/add.blade.php
+++ resources/views/goal/add.blade.php
@@ -24,6 +24,16 @@
 
         <p class="text-center mt-5">{{ __('messages.AddFoodToGoalDescription') }}</p>
 
+        @if($errors->any())
+
+        @foreach($errors->all() as $error)
+        <div class="d-flex justify-content-center">
+            <strong class="text-danger">{{ $error }}</strong>
+        </div>
+        @endforeach
+
+        @endif
+
         <table class="table">
             <thead>
                 <tr>
@@ -44,14 +54,14 @@
                 <tr>
                     <form method="POST" action="{{ route('goal.addfood', ['type' => $type]) }}">
                         @csrf
-                        <td><input type="text" class="form-control" name="name" value="{{__($value->name)}}" step="any" readonly></th>
-                        <td><input id="quantityGramsId-{{__($key)}}" type="number" class="form-control" name="quantity_grams" value="{{__($value->quantity_grams)}}" step="any"></td>
-                        <td><input id="quantityCalorieId-{{__($key)}}" type="number" class="form-control border-0" name="calories" value="{{__($value->calories)}}" step="any" readonly></td>
-                        <td><input id="quantityCarbohydrateId-{{__($key)}}" type="number" class="form-control border-0" name="carbohydrate" value="{{__($value->carbohydrate)}}" step="any" readonly></td>
-                        <td><input id="quantityProteinId-{{__($key)}}" type="number" class="form-control border-0" name="protein" value="{{__($value->protein)}}" step="any" readonly></td>
-                        <td><input id="quantityTotalFatId-{{__($key)}}" type="number" class="form-control border-0" name="total_fat" value="{{__($value->total_fat)}}" step="any" readonly></td>
-                        <td><input id="quantitySaturatedFatId-{{__($key)}}" type="number" class="form-control border-0" name="saturated_fat" value="{{__($value->saturated_fat)}}" step="any" readonly></td>
-                        <td><input id="quantityTransFatId-{{__($key)}}" type="number" class="form-control border-0" name="trans_fat" value="{{__($value->trans_fat)}}" step="any" readonly></td>
+                        <td><input type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{__($value->name)}}" step="any" readonly></th>
+                        <td><input id="quantityGramsId-{{__($key)}}" type="number" class="form-control @error('quantity_grams') is-invalid @enderror" name="quantity_grams" value="{{__($value->quantity_grams)}}" step="any"></td>
+                        <td><input id="quantityCalorieId-{{__($key)}}" type="number" class="form-control border-0 @error('calories') is-invalid @enderror" name="calories" value="{{__($value->calories)}}" step="any" readonly></td>
+                        <td><input id="quantityCarbohydrateId-{{__($key)}}" type="number" class="form-control border-0 @error('carbohydrate') is-invalid @enderror" name="carbohydrate" value="{{__($value->carbohydrate)}}" step="any" readonly></td>
+                        <td><input id="quantityProteinId-{{__($key)}}" type="number" class="form-control border-0 @error('protein') is-invalid @enderror" name="protein" value="{{__($value->protein)}}" step="any" readonly></td>
+                        <td><input id="quantityTotalFatId-{{__($key)}}" type="number" class="form-control border-0 @error('total_fat') is-invalid @enderror" name="total_fat" value="{{__($value->total_fat)}}" step="any" readonly></td>
+                        <td><input id="quantitySaturatedFatId-{{__($key)}}" type="number" class="form-control border-0 @error('saturated_fat') is-invalid @enderror" name="saturated_fat" value="{{__($value->saturated_fat)}}" step="any" readonly></td>
+                        <td><input id="quantityTransFatId-{{__($key)}}" type="number" class="form-control border-0 @error('trans_fat') is-invalid @enderror" name="trans_fat" value="{{__($value->trans_fat)}}" step="any" readonly></td>
 
                         <td><button class="btn btn-primary">{{ __('messages.Add') }}</button></td>
                     </form>
