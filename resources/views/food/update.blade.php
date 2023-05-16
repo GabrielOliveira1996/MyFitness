6f6293c9f4293b97efc45d34e7ed8618f895861d
Gabriel <dev.gabriel1996@gmail.com>
2023-05-16 10:50:48 -0300

    fix: :hammer: modal de errors removido
    erro agora é mostrado em input, mensages são exibidas abaixo dos inputs com falhas na validação

--- resources/views/food/update.blade.php
+++ resources/views/food/update.blade.php
@@ -3,24 +3,13 @@
 
 @section('content')
 
-@if($errors->any())
-        <div id="modalErrorsValidateFood" class="fixed-bottom col-lg-4 alert alert-danger m-3">
-            <div class="text-white d-flex flex-row-reverse">
-                <a href="" onclick="closeModalErrorsValidateFood()" style="text-decoration:none;">X</a>
-            </div>
-            @foreach($errors->all() as $error)
-                {{$error}} </br>
-            @endforeach
-        </div>
-@endif
-
 <div class="container d-flex justify-content-center py-5">
 
     <div class="row">
         <p class="text-center col-lg-12">{{ __('messages.UpdateFoodDescription') }}</p>
 
         <form method="POST" autocomplete="off">
-            @csrf            
+            @csrf
             <table class="table mt-3">
                 <thead>
                     <tr>
@@ -37,64 +26,103 @@
                 <tbody>
                     <tr>
                         <td>
-                            <input type="text" class="form-control bg-light" name="name" value="{{ $food->name; }}" step="any" readonly>
-                        </th>
+                            <input type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ $food->name; }}" step="any">
+                            @error('name')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
                         <td>
                             <div class="input-group mb-3">
-                                <input type="number" class="form-control" name="quantity_grams" value="{{ $food->quantity_grams; }}">
+                                <input type="number" class="form-control @error('quantity_grams') is-invalid @enderror" name="quantity_grams" value="{{ $food->quantity_grams; }}">
                                 <div class="input-group-append">
                                     <span class="input-group-text" id="basic-addon2">g</span>
                                 </div>
+                                @error('quantity_grams')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
                             </div>
                         </td>
                         <td>
                             <div class="input-group mb-3">
-                                <input type="number" class="form-control" name="calories" value="{{ $food->calories; }}">
+                                <input type="number" class="form-control @error('calories') is-invalid @enderror" name="calories" value="{{ $food->calories; }}">
                                 <div class="input-group-append">
                                     <span class="input-group-text" id="basic-addon2">Kcal</span>
                                 </div>
+                                @error('calories')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
                             </div>
                         </td>
                         <td>
                             <div class="input-group mb-3">
-                                <input type="number" class="form-control" name="carbohydrate" value="{{ $food->carbohydrate; }}">
+                                <input type="number" class="form-control @error('carbohydrate') is-invalid @enderror" name="carbohydrate" value="{{ $food->carbohydrate; }}">
                                 <div class="input-group-append">
                                     <span class="input-group-text" id="basic-addon2">g</span>
                                 </div>
+                                @error('carbohydrate')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
                             </div>
                         </td>
                         <td>
                             <div class="input-group mb-3">
-                                <input type="number" class="form-control" name="protein" value="{{ $food->protein; }}">
+                                <input type="number" class="form-control @error('protein') is-invalid @enderror" name="protein" value="{{ $food->protein; }}">
                                 <div class="input-group-append">
                                     <span class="input-group-text" id="basic-addon2">g</span>
                                 </div>
+                                @error('protein')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
                             </div>
                         </td>
                         <td>
                             <div class="input-group mb-3">
-                                <input type="number" class="form-control" name="total_fat" value="{{ $food->total_fat; }}">
+                                <input type="number" class="form-control @error('total_fat') is-invalid @enderror" name="total_fat" value="{{ $food->total_fat; }}">
                                 <div class="input-group-append">
                                     <span class="input-group-text" id="basic-addon2">g</span>
                                 </div>
+                                @error('total_fat')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
                             </div>
                         </td>
                         <td>
                             <div class="input-group mb-3">
-                                <input type="number" class="form-control" name="saturated_fat" value="{{ $food->saturated_fat; }}">
+                                <input type="number" class="form-control @error('saturated_fat') is-invalid @enderror" name="saturated_fat" value="{{ $food->saturated_fat; }}">
                                 <div class="input-group-append">
                                     <span class="input-group-text" id="basic-addon2">g</span>
                                 </div>
+                                @error('saturated_fat')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
                             </div>
                         </td>
                         <td>
                             <div class="input-group mb-3">
-                                <input type="number" class="form-control" name="trans_fat" value="{{ $food->trans_fat; }}">
+                                <input type="number" class="form-control @error('trans_fat') is-invalid @enderror" name="trans_fat" value="{{ $food->trans_fat; }}">
                                 <div class="input-group-append">
                                     <span class="input-group-text" id="basic-addon2">g</span>
                                 </div>
+                                @error('trans_fat')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
                             </div>
-                        </td>                    
+                        </td>
                     </tr>
                 </tbody>
             </table>
@@ -111,7 +139,7 @@
                 <button type="submit" class="btn btn-primary col-lg-12">
                     {{ __('messages.Update') }}
                 </button>
-            </div>    
+            </div>
         </form>
     </div>
 </div>
