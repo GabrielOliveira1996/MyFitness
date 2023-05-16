f5d9819c240850d5d1dfca4451cb4a4211b383ba
Gabriel <dev.gabriel1996@gmail.com>
2023-05-16 12:51:24 -0300

    feat: adicionado listagem de erros de validação, erros são exibidos abaixo de inputs

--- resources/views/goal/update.blade.php
+++ resources/views/goal/update.blade.php
@@ -25,61 +25,101 @@
                 <tbody>
                     <tr>
                         <td>
-                            <input type="text" class="form-control bg-light" name="name" value="{{ $food->name }}" step="any" readonly>
+                            <input type="text" class="form-control bg-light @error('name') is-invalid @enderror" name="name" value="{{ $food->name }}" step="any" readonly>
+                            @error('name')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
                         <td>
                             <div class="input-group mb-3">
-                                <input id="quantityGramsId-1" type="number" class="form-control" name="quantity_grams" value="{{ $food->quantity_grams }}" step="any">
+                                <input id="quantityGramsId-1" type="number" class="form-control @error('quantity_grams') is-invalid @enderror" name="quantity_grams" value="{{ $food->quantity_grams }}" step="any">
                                 <div class="input-group-append">
                                     <span class="input-group-text" id="basic-addon2">g</span>
                                 </div>
+                                @error('quantity_grams')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
                             </div>
                         </td>
                         <td>
                             <div class="input-group mb-3">
-                                <input id="quantityCalorieId-1" type="number" class="form-control" name="calories" value="{{ $food->calories }}" step="any">
+                                <input id="quantityCalorieId-1" type="number" class="form-control @error('calories') is-invalid @enderror" name="calories" value="{{ $food->calories }}" step="any">
                                 <div class="input-group-append">
                                     <span class="input-group-text" id="basic-addon2">Kcal</span>
                                 </div>
+                                @error('calories')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
                             </div>
                         </td>
                         <td>
                             <div class="input-group mb-3">
-                                <input id="quantityCarbohydrateId-1" type="number" class="form-control" name="carbohydrate" value="{{ $food->carbohydrate }}" step="any">
+                                <input id="quantityCarbohydrateId-1" type="number" class="form-control @error('carbohydrate') is-invalid @enderror" name="carbohydrate" value="{{ $food->carbohydrate }}" step="any">
                                 <div class="input-group-append">
                                     <span class="input-group-text" id="basic-addon2">g</span>
                                 </div>
+                                @error('carbohydrate')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
                             </div>
                         </td>
                         <td>
                             <div class="input-group mb-3">
-                                <input id="quantityProteinId-1" type="number" class="form-control" name="protein" value="{{ $food->protein }}" step="any">
+                                <input id="quantityProteinId-1" type="number" class="form-control @error('protein') is-invalid @enderror" name="protein" value="{{ $food->protein }}" step="any">
                                 <div class="input-group-append">
                                     <span class="input-group-text" id="basic-addon2">g</span>
                                 </div>
+                                @error('protein')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
                             </div>
                         </td>
                         <td>
                             <div class="input-group mb-3">
-                                <input id="quantityTotalFatId-1" type="number" class="form-control" name="total_fat" value="{{ $food->total_fat }}" step="any">
+                                <input id="quantityTotalFatId-1" type="number" class="form-control @error('total_fat') is-invalid @enderror" name="total_fat" value="{{ $food->total_fat }}" step="any">
                                 <div class="input-group-append">
                                     <span class="input-group-text" id="basic-addon2">g</span>
                                 </div>
+                                @error('total_fat')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
                             </div>
                         </td>
                         <td>
                             <div class="input-group mb-3">
-                                <input id="quantitySaturatedFatId-1" type="number" class="form-control" name="saturated_fat" value="{{ $food->saturated_fat }}" step="any">
+                                <input id="quantitySaturatedFatId-1" type="number" class="form-control @error('saturated_fat') is-invalid @enderror" name="saturated_fat" value="{{ $food->saturated_fat }}" step="any">
                                 <div class="input-group-append">
                                     <span class="input-group-text" id="basic-addon2">g</span>
                                 </div>
+                                @error('saturated_fat')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
                             </div>
                         </td>
                         <td>
                             <div class="input-group mb-3">
-                                <input id="quantityTransFatId-1" type="number" class="form-control" name="trans_fat" value="{{ $food->trans_fat }}" step="any">
+                                <input id="quantityTransFatId-1" type="number" class="form-control @error('trans_fat') is-invalid @enderror" name="trans_fat" value="{{ $food->trans_fat }}" step="any">
                                 <div class="input-group-append">
                                     <span class="input-group-text" id="basic-addon2">g</span>
                                 </div>
+                                @error('trans_fat')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
                             </div>
                         </td>
                     </tr>
