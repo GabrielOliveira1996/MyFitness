a07b6690e47e828997a86633c75518d9cee29fc0
Gabriel <dev.gabriel1996@gmail.com>
2023-05-16 10:52:20 -0300

    feat: adicionado validação para atualização (update) de alimentos

--- app/Validator/FoodValidator.php
+++ app/Validator/FoodValidator.php
@@ -10,7 +10,32 @@ class FoodValidator
     public function create($food)
     {
         $rules = [
-            'name' => 'required|alpha',
+            'name' => 'required',
+            'quantity_grams' => 'required|numeric|min:0',
+            'calories' => 'required|numeric|min:0',
+            'carbohydrate' => 'required|numeric|min:0',
+            'protein' => 'required|numeric|min:0',
+            'total_fat' => 'required|numeric|min:0',
+            'saturated_fat' => 'required|numeric|min:0',
+            'trans_fat' => 'required|numeric|min:0'
+        ];
+
+        $validator = Validator::make($food, $rules);
+        if ($validator->fails()) {
+            $errors = $validator->messages();
+            throw new ValidatorException(
+                'Fields were not filled in, check if the request receives data.',
+                422,
+                null,
+                $errors
+            );
+        }
+    }
+
+    public function update($food)
+    {
+        $rules = [
+            'name' => 'required',
             'quantity_grams' => 'required|numeric|min:0',
             'calories' => 'required|numeric|min:0',
             'carbohydrate' => 'required|numeric|min:0',
