dc1ea2beac5454af67804ae2752bfe91e02007f3
Gabriel <dev.gabriel1996@gmail.com>
2023-05-16 12:52:56 -0300

    feat: parametro type_of_meal adicionado a chamada de rota goal.update, parâmetro recebe o tipo do alimento que foi selecionado para edição

--- resources/views/goal/index.blade.php
+++ resources/views/goal/index.blade.php
@@ -141,7 +141,7 @@
                     <td><input type="number" class="form-control bg-light" name="protein" value="{{__($breakfast->protein)}}" step="any" disabled></td>
                     <td><input type="number" class="form-control bg-light" name="total_fat" value="{{__($breakfast->total_fat)}}" step="any" disabled></td>
                     <td>
-                        <a href="{{ route('goal.update', ['id' => $breakfast->id]) }}" class="btn btn-primary">
+                        <a href="{{ route('goal.update', ['id' => $breakfast->id, 'type_of_meal' => $breakfast->type_of_meal]) }}" class="btn btn-primary">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
                                 <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z" />
                             </svg>
@@ -198,7 +198,7 @@
                     <td><input type="number" class="form-control bg-light" name="protein" value="{{__($lunch->protein)}}" step="any" disabled></td>
                     <td><input type="number" class="form-control bg-light" name="total_fat" value="{{__($lunch->total_fat)}}" step="any" disabled></td>
                     <td>
-                        <a href="{{ route('goal.update', ['id' => $lunch->id]) }}" class="btn btn-primary">
+                        <a href="{{ route('goal.update', ['id' => $lunch->id, 'type_of_meal' => $breakfast->type_of_meal]) }}" class="btn btn-primary">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
                                 <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z" />
                             </svg>
@@ -255,7 +255,7 @@
                     <td><input type="number" class="form-control bg-light" name="protein" value="{{__($snack->protein)}}" step="any" disabled></td>
                     <td><input type="number" class="form-control bg-light" name="total_fat" value="{{__($snack->total_fat)}}" step="any" disabled></td>
                     <td>
-                        <a href="{{ route('goal.update', ['id' => $snack->id]) }}" class="btn btn-primary">
+                        <a href="{{ route('goal.update', ['id' => $snack->id, 'type_of_meal' => $breakfast->type_of_meal]) }}" class="btn btn-primary">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
                                 <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z" />
                             </svg>
@@ -312,7 +312,7 @@
                     <td><input type="number" class="form-control bg-light" name="protein" value="{{__($dinner->protein)}}" step="any" disabled></td>
                     <td><input type="number" class="form-control bg-light" name="total_fat" value="{{__($dinner->total_fat)}}" step="any" disabled></td>
                     <td>
-                        <a href="{{ route('goal.update', ['id' => $dinner->id]) }}" class="btn btn-primary">
+                        <a href="{{ route('goal.update', ['id' => $dinner->id, 'type_of_meal' => $breakfast->type_of_meal]) }}" class="btn btn-primary">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
                                 <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z" />
                             </svg>
@@ -369,7 +369,7 @@
                     <td><input type="number" class="form-control bg-light" name="protein" value="{{__($preWorkout->protein)}}" step="any" disabled></td>
                     <td><input type="number" class="form-control bg-light" name="total_fat" value="{{__($preWorkout->total_fat)}}" step="any" disabled></td>
                     <td>
-                        <a href="{{ route('goal.update', ['id' => $preWorkout->id]) }}" class="btn btn-primary">
+                        <a href="{{ route('goal.update', ['id' => $preWorkout->id, 'type_of_meal' => $breakfast->type_of_meal]) }}" class="btn btn-primary">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
                                 <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z" />
                             </svg>
@@ -426,7 +426,7 @@
                     <td><input type="number" class="form-control bg-light" name="protein" value="{{__($postWorkout->protein)}}" step="any" disabled></td>
                     <td><input type="number" class="form-control bg-light" name="total_fat" value="{{__($postWorkout->total_fat)}}" step="any" disabled></td>
                     <td>
-                        <a href="{{ route('goal.update', ['id' => $postWorkout->id]) }}" class="btn btn-primary">
+                        <a href="{{ route('goal.update', ['id' => $postWorkout->id, 'type_of_meal' => $breakfast->type_of_meal]) }}" class="btn btn-primary">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
                                 <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z" />
                             </svg>
