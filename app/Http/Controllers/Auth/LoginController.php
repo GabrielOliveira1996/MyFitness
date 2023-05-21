2d3251117165e4fe3d6c8ded1baf81309f7abe39
Gabriel <dev.gabriel1996@gmail.com>
2023-05-21 00:44:59 -0300

    fix: :hammer: controller de login de usuário simplificado

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,36 +5,36 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
     use AuthenticatesUsers;
 
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
     protected $redirectTo = RouteServiceProvider::HOME;
-    private $_request;
 
-    public function __construct(Request $request)
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
     {
         $this->middleware('guest')->except('logout');
-        $this->_request = $request;
-    }
-
-    public function login()
-    {
-        $user = $this->_request->only('email', 'password');
-        if (Auth::attempt($user)) {
-            return redirect()->intended('/home');
-        }
-        return redirect()->back()->withErrors(['email' => 'Credenciais inválidas.']);
-    }
-
-    public function logout()
-    {
-        Auth::logout();
-        $this->_request->session()->invalidate();
-        $this->_request->session()->regenerateToken();
-        return redirect('/');
     }
 }
