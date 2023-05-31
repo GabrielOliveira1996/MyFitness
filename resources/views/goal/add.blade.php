1b20a37efdf0e8c9a051fea10f2ede86658e216b
Gabriel Oliveira <gab-oliveira96@hotmail.com>
2023-05-30 21:38:56 -0300

    style: :art: Adicionada imagem 'seus-alimentos.png' e removida a tabela da página. Agora, os inputs são exibidos em linha (row).

--- resources/views/goal/add.blade.php
+++ resources/views/goal/add.blade.php
@@ -4,78 +4,183 @@
 
 
 <div class="container py-5">
+
     <div class="row">
 
-        <p class="text-center">{{ __('messages.SearchDescription') }}</p>
+    <div class="row mt-3 mx-5">
+            <div class="row mb-5">
+                <img src="{{ asset('img/seus-alimentos.png') }}" class="col-lg-5">
+                <div class="col-lg-5 mt-5">
+                    <h1 class="mt-5 fw-bold">
+                        {{ __('messages.AddFoodToGoalDescription') }}
+                    </h1>
+                    <h5 class="mt-2">
+                    {{ __('messages.AddFoodToGoalDescription') }}
+                    </h5>
+                </div>
+            </div>
+        </div>
 
         <form method="post" autocomplete="off">
             @csrf
             <div class="row d-flex justify-content-center">
-                <div class="col-md-5">
-                    <input type="text" name="name" class="form-control">
+                <div class="col-md-8 inputBox">
+                    <input type="text" name="name" required>
+                    <label for="name" class="labelInput">{{__('messages.SearchFood1')}}</label>
                 </div>
             </div>
 
             <div class="row d-flex justify-content-center">
-                <button class="btn btn-primary col-md-3 mt-3">{{ __('messages.Search') }}</button>
+                <button class="btn btn-primary col-md-3 mt-3">{{__('messages.SearchFood')}}</button>
             </div>
 
         </form>
 
-        <p class="text-center mt-5">{{ __('messages.AddFoodToGoalDescription') }}</p>
+        <hr class="col-md-12 mt-3"/>
 
-        @if($errors->any())
-
-        @foreach($errors->all() as $error)
-        <div class="d-flex justify-content-center">
-            <strong class="text-danger">{{ $error }}</strong>
-        </div>
-        @endforeach
-
-        @endif
-
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">{{ __('messages.Name') }}</th>
-                    <th scope="col">{{ __('messages.AmountInGrams') }}</th>
-                    <th scope="col">{{ __('messages.Calories') }}</th>
-                    <th scope="col">{{ __('messages.Carbohydrate') }}</th>
-                    <th scope="col">{{ __('messages.Protein') }}</th>
-                    <th scope="col">{{ __('messages.Fat') }}</th>
-                    <th scope="col">{{ __('messages.SaturatedFat') }}</th>
-                    <th scope="col">{{ __('messages.TransFat') }}</th>
-                    <th scope="col">{{ __('messages.Add') }}</th>
-                </tr>
-            </thead>
-            <tbody>
-                @if(!empty($foods))
+        <div class="row">
+            @if(!empty($foods))
                 @foreach($foods as $key => $value)
-                <tr>
-                    <form method="POST" action="{{ route('goal.addfood', ['type' => $type]) }}">
-                        @csrf
-                        <td><input type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{__($value->name)}}" step="any" readonly></th>
-                        <td><input id="quantityGramsId-{{__($key)}}" type="number" class="form-control @error('quantity_grams') is-invalid @enderror" name="quantity_grams" value="{{__($value->quantity_grams)}}" step="any"></td>
-                        <td><input id="quantityCalorieId-{{__($key)}}" type="number" class="form-control border-0 @error('calories') is-invalid @enderror" name="calories" value="{{__($value->calories)}}" step="any" readonly></td>
-                        <td><input id="quantityCarbohydrateId-{{__($key)}}" type="number" class="form-control border-0 @error('carbohydrate') is-invalid @enderror" name="carbohydrate" value="{{__($value->carbohydrate)}}" step="any" readonly></td>
-                        <td><input id="quantityProteinId-{{__($key)}}" type="number" class="form-control border-0 @error('protein') is-invalid @enderror" name="protein" value="{{__($value->protein)}}" step="any" readonly></td>
-                        <td><input id="quantityTotalFatId-{{__($key)}}" type="number" class="form-control border-0 @error('total_fat') is-invalid @enderror" name="total_fat" value="{{__($value->total_fat)}}" step="any" readonly></td>
-                        <td><input id="quantitySaturatedFatId-{{__($key)}}" type="number" class="form-control border-0 @error('saturated_fat') is-invalid @enderror" name="saturated_fat" value="{{__($value->saturated_fat)}}" step="any" readonly></td>
-                        <td><input id="quantityTransFatId-{{__($key)}}" type="number" class="form-control border-0 @error('trans_fat') is-invalid @enderror" name="trans_fat" value="{{__($value->trans_fat)}}" step="any" readonly></td>
+                    
+                <div class="col-md-4 p-1 mt-3">
+                        <form method="POST" action="{{ route('goal.addfood', ['type' => $type]) }}">
+                            @csrf
+                            <div class="card m-2 p-0">
+                                <div class="card-header">
+                                    <input type="text" 
+                                            class="emptyInput @error('name') is-invalid @enderror" 
+                                            name="name" 
+                                            value="{{__($value->name)}}" 
+                                            step="any"
+                                            required 
+                                            readonly
+                                    />
+                                </div>
+                                <div class="card-body">
+                                    <div class="row">
+                                        <div class="inputBox col-md-6">
+                                            <input id="quantityGramsId-{{__($key)}}" 
+                                                    type="number" 
+                                                    class="@error('quantity_grams') is-invalid @enderror" 
+                                                    name="quantity_grams" 
+                                                    value="{{__($value->quantity_grams)}}" 
+                                                    step="any"
+                                                    required 
+                                            />
+                                            <label for="quantity_grams" class="labelInput">{{ __('messages.AmountInGrams') }}</label>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            {{ __('messages.Calories') }}
+                                        </div>
+                                        <div class="col-md-6">
+                                            <input id="quantityCalorieId-{{__($key)}}" 
+                                                    type="number" 
+                                                    class="emptyInput border-0 @error('calories') is-invalid @enderror" 
+                                                    name="calories" 
+                                                    value="{{__($value->calories)}}" 
+                                                    step="any" 
+                                                    readonly
+                                            />
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            {{ __('messages.Carbohydrate') }}
+                                        </div>
+                                        <div class="col-md-6">
+                                            <input id="quantityCarbohydrateId-{{__($key)}}" 
+                                                    type="number" 
+                                                    class="emptyInput border-0 @error('carbohydrate') is-invalid @enderror" 
+                                                    name="carbohydrate" 
+                                                    value="{{__($value->carbohydrate)}}" 
+                                                    step="any" 
+                                                    readonly 
+                                            />
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            {{ __('messages.Protein') }}
+                                        </div>
+                                        <div class="col-md-6">
+                                            <input id="quantityProteinId-{{__($key)}}" 
+                                                    type="number" 
+                                                    class="emptyInput border-0 @error('protein') is-invalid @enderror" 
+                                                    name="protein" 
+                                                    value="{{__($value->protein)}}" 
+                                                    step="any"
+                                                    readonly 
+                                            />
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            {{ __('messages.Fat') }}
+                                        </div>
+                                        <div class="col-md-6">
+                                            <input id="quantityTotalFatId-{{__($key)}}" 
+                                                    type="number" 
+                                                    class="emptyInput border-0 @error('total_fat') is-invalid @enderror" 
+                                                    name="total_fat" 
+                                                    value="{{__($value->total_fat)}}" 
+                                                    step="any" 
+                                                    readonly 
+                                            />
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            {{ __('messages.SaturatedFat') }}
+                                        </div>
+                                        <div class="col-md-6">
+                                            <input id="quantitySaturatedFatId-{{__($key)}}" 
+                                                    type="number" 
+                                                    class="emptyInput border-0 @error('saturated_fat') is-invalid @enderror" 
+                                                    name="saturated_fat" 
+                                                    value="{{__($value->saturated_fat)}}" 
+                                                    step="any" 
+                                                    readonly 
+                                            />
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            {{ __('messages.TransFat') }}
+                                        </div>
+                                        <div class="col-md-6">
+                                            <input id="quantityTransFatId-{{__($key)}}" 
+                                                    type="number" 
+                                                    class="emptyInput border-0 @error('trans_fat') is-invalid @enderror" 
+                                                    name="trans_fat" 
+                                                    value="{{__($value->trans_fat)}}" 
+                                                    step="any" 
+                                                    readonly 
+                                            />
+                                        </div>
+                                    </div>
 
-                        <td><button class="btn btn-primary">{{ __('messages.Add') }}</button></td>
-                    </form>
-                </tr>
+                                    <hr class="col-lg-12"/>
+
+                                    <div class="row">
+                                        <div class="col-md-5 mt-1">
+                                            <button class="btn btn-primary">{{ __('messages.Add') }}</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        
+                        </form>
+                    </div>
                 @endforeach
-                @endif
-            </tbody>
-        </table>
-
+            @endif
+        </div>
     </div>
 
     <div class="d-flex justify-content-center mt-5">
         @if(!empty($foods))
-        {{$foods->links()}}
+            {{ $foods->links() }}
         @endif
     </div>
 
