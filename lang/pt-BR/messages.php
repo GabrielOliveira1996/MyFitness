9aae5adccf16e1dc199fa29fd687577993e94ba3
Gabriel Oliveira <gab-oliveira96@hotmail.com>
2023-06-11 12:27:40 -0300

    fix: :hammer: Aprimorada a descrição de 'AddFoodToGoalDescription'.

--- lang/pt-BR/messages.php
+++ lang/pt-BR/messages.php
@@ -115,7 +115,7 @@ return [
 
     'UpdateFoodGoalDescription' => 'Você selecionou um alimento da sua lista de metas, nesta página você pode realizar alterações no alimento, caso as informações não estejam preenchidas de forma correta.',
 
-    'AddFoodToGoalDescription' => 'Adicione alimentos para sua meta do dia.',
+    'AddFoodToGoalDescription' => 'Nesta página, você pode selecionar os alimentos que deseja adicionar à sua meta. Isso irá impactar nos resultados exibidos na sua meta, proporcionando um auxílio significativo em seu progresso.',
 
     'SearchDescription' => 'Busque por alimentos.',
 
