82cbd29a30f88b6514a3c2e2f4ead1eb60d54140
Gabriel Oliveira <gab-oliveira96@hotmail.com>
2023-08-07 10:17:06 -0300

    refactor: :hammer: arquivos de autenticação foram passados para a pasta 'user'.

--- resources/views/user/email.blade.php
+++ resources/views/user/email.blade.php
@@ -24,7 +24,7 @@
                         <div class="row">
                             <div class="col-md-12">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
+                                
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
