2609138f2855708cfee39a0ec373ae2062dff305
Gabriel Oliveira <gab-oliveira96@hotmail.com>
2023-08-10 14:46:29 -0300

    feat: Validação de usuário
    Adicionada validação para o registro de uma nova senha. A função recebe os dados da requisição, realiza as verificações necessárias e, em caso de erro, trata a exceção retornando uma mensagem de erro.

--- app/Validator/UserValidator.php
+++ app/Validator/UserValidator.php
@@ -63,4 +63,26 @@ class UserValidator
             );
         }
     }
+
+    public function resetPassword($user)
+    {
+        $rules = [
+            'token' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|min:8|confirmed',
+        ];
+        $messages = [
+            'password.regex' => 'The password field format is invalid. The password must contain at least one uppercase letter, one lowercase letter, one number and one special character.'
+        ];
+        $validator = Validator::make($user, $rules, $messages);
+        if ($validator->fails()) {
+            $errors = $validator->messages();
+            throw new ValidatorException(
+                'Fields were not filled in, check if the request receives data.',
+                422,
+                null,
+                $errors
+            );
+        }
+    }
 }
