67a11e9dc7753d268a585b0b96061d072afcc5f4
Gabriel Oliveira <gab-oliveira96@hotmail.com>
2023-08-11 11:02:08 -0300

    feat: :sparkles: Exceção em 'recoverPassword'
    Foi incorporada uma exceção à função 'recoverPassword', destinada a capturar erros provenientes da interação com a API do SendinBlue. Além disso, implementou-se um registro mais detalhado desses erros no log da aplicação.

--- app/Mail/MailProvider.php
+++ app/Mail/MailProvider.php
@@ -6,6 +6,8 @@ use Illuminate\Support\Facades\Password;
 use SendinBlue\Client\Configuration;
 use SendinBlue\Client\Api\TransactionalEmailsApi;
 use SendinBlue\Client\Model\SendSmtpEmail;
+use SendinBlue\Client\ApiException;
+use Illuminate\Support\Facades\Log;
 
 class MailProvider{
     private $_password;
@@ -26,24 +28,32 @@ class MailProvider{
     }
 
     public function recoverPassword($user){
-        $token = $this->_password::createToken($user);
-        $resetLink = route('password.reset', [
-            'token' => $token,
-            'email' => $user['email']
-        ]);
-        $sendinblueApiKey = env('SENDINBLUE_API_KEY');
-        $configuration = $this->_configuration->getDefaultConfiguration()->setApiKey('api-key', $sendinblueApiKey);
-        $apiInstance = new $this->_transictionalEmailsApi(new \GuzzleHttp\Client(), $configuration);
-        $sendSmtpEmail = new $this->_sendSmtpEmail([
-            'sender' => ['name' => 'MyFitness', 'email' => 'myfitness.assistance@gmail.com'],
-            'to' => [['email' => $user['email']]],
-            'params' => [
-                'resetLink' => $resetLink,
-            ],
-            'subject' => 'Recuperação de senha.',
-            'templateId' => 1,
-        ]);
-        $result = $apiInstance->sendTransacEmail($sendSmtpEmail);
+        try{
+            $token = $this->_password::createToken($user);
+            $resetLink = route('password.reset', [
+                'token' => $token,
+                'email' => $user['email']
+            ]);
+            $sendinblueApiKey = env('SENDINBLUE_API_KEY');
+            $configuration = $this->_configuration->getDefaultConfiguration()->setApiKey('api-key', $sendinblueApiKey);
+            $apiInstance = new $this->_transictionalEmailsApi(new \GuzzleHttp\Client(), $configuration);
+            $sendSmtpEmail = new $this->_sendSmtpEmail([
+                'sender' => ['name' => 'MyFitness', 'email' => 'myfitness.assistance@gmail.com'],
+                'to' => [['email' => $user['email']]],
+                'params' => [
+                    'resetLink' => $resetLink,
+                ],
+                'subject' => 'Recuperação de senha.',
+                'templateId' => 1,
+            ]);
+            $result = $apiInstance->sendTransacEmail($sendSmtpEmail);
+            return back()->withErrors(['status' => 'Por favor, verifique o seu e-mail para continuar.']);
+        }catch(ApiException $e){
+            if($e->getCode() == 401){
+                Log::error('Erro de autenticação ao enviar email.' . $e->getMessage());
+                return back()->withErrors(['status' => 'Erro no envio de e-mail.', 'email' => 'Houve um problema no envio do e-mail. Nossa equipe foi notificada e está trabalhando para resolver o problema.']);
+            }
+        }
     }
 
 }
