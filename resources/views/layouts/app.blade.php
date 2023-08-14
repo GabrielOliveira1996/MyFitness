efcb1ede65e055c4daa10b5cc43165487b888bf3
Gabriel Oliveira <gab-oliveira96@hotmail.com>
2023-08-14 12:04:46 -0300

    style: :art: Alterações no 'layout.app'
    Navbar e footer foram ocultados nas páginas 'login' e 'register'. Link para utilização de icones foi adicionado.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,6 +23,9 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
+    <!-- Icons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+
     <!--Bootstrap-->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
@@ -38,96 +41,127 @@
 
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-primary shadow-sm">
-            <div class="container">
-                <a class="navbar-brand text-light" href="{{ url('/') }}">
-                    <h4>{{ config('app.name', 'MyFitness') }}</h4>
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+        @if(Route::getFacadeRoot()->current()->uri() !== 'login' && Route::getFacadeRoot()->current()->uri() !== 'register')
+            <nav class="navbar navbar-expand-md navbar-light bg-primary shadow-sm">
+                <div class="container">
+                    <a class="navbar-brand text-light" href="{{ url('/') }}">
+                        <h4 class="brand">{{ config('app.name', 'MYFITNESS') }}</h4>
+                    </a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <!-- Left Side Of Navbar -->
+                        <ul class="navbar-nav me-auto">
 
-                    </ul>
+                        </ul>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                        <li class="nav-item">
-                            <a class="nav-link text-light" href="{{ route('login') }}">{{ __('messages.Login') }}</a>
-                        </li>
+                        <!-- Right Side Of Navbar -->
+                        <ul class="navbar-nav ms-auto">
+                            <!-- Authentication Links -->
+                            @guest
+                            <li class="nav-item">
+                                <a class="nav-link text-light" href="{{ route('login') }}">{{ __('messages.Login') }}</a>
+                            </li>
 
-                        <li class="nav-item">
-                            <a class="nav-link text-light" href="{{ route('register') }}">{{ __('messages.Register') }}</a>
-                        </li>
-                        @else
+                            <li class="nav-item">
+                                <a class="nav-link text-light" href="{{ route('register') }}">{{ __('messages.Register') }}</a>
+                            </li>
+                            @else
 
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle text-light" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ __('messages.HelloMessage') }}
-                                {{ Auth::user()->name }}
-                            </a>
-                            <div class="dropdown-menu">
-                                <a class="dropdown-item" href="{{ route('profile') }}">
-                                    {{ __('messages.Profile') }}
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle text-light" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ __('messages.HelloMessage') }}
+                                    {{ Auth::user()->name }}
                                 </a>
-                                <a class="dropdown-item" href="{{ route('food.all') }}">
-                                    {{ __('messages.MyFoods') }}
+                                <div class="dropdown-menu">
+                                    <a class="dropdown-item" href="{{ route('profile') }}">
+                                        {{ __('messages.Profile') }}
+                                    </a>
+                                    <a class="dropdown-item" href="{{ route('food.all') }}">
+                                        {{ __('messages.MyFoods') }}
+                                    </a>
+                                </div>
+                            </li>
+
+                            <!-- Separador -->
+
+                            <li class="nav-item">
+                                <a class="nav-link text-light" href="{{ route('goal.index') }}" role="button">
+                                    {{ __('messages.MyGoals') }}
                                 </a>
-                            </div>
-                        </li>
+                            </li>
 
-                        <!-- Separador -->
+                            <li class="nav-item">
+                                <a class="nav-link text-light" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                        document.getElementById('logout-form').submit();">
+                                    {{ __('messages.SignOut') }}
+                                </a>
 
-                        <li class="nav-item">
-                            <a class="nav-link text-light" href="{{ route('goal.index') }}" role="button">
-                                {{ __('messages.MyGoals') }}
-                            </a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link text-light" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                    document.getElementById('logout-form').submit();">
-                                {{ __('messages.SignOut') }}
-                            </a>
-
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                @csrf
-                            </form>
-                        </li>
-                        @endguest
-                    </ul>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </li>
+                            @endguest
+                        </ul>
+                    </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
+        @endif
 
         <main class="">
             @yield('content')
         </main>
 
+        @if(Route::getFacadeRoot()->current()->uri() !== 'login' && Route::getFacadeRoot()->current()->uri() !== 'register')
+            <footer class="bg-light text-center text-white">
+            <!-- Grid container -->
+            <div class="container p-4 pb-0">
+                <!-- Section: Social media -->
+                <section class="mb-4">
+                <!-- Google -->
+                <a
+                    class="btn text-white btn-floating m-1"
+                    style="background-color: #dd4b39;"
+                    href="#!"
+                    role="button"
+                    target="__blank"
+                    ><i class="fab fa-google"></i
+                ></a>
+                <!-- Linkedin -->
+                <a
+                    class="btn text-white btn-floating m-1"
+                    style="background-color: #0082ca;"
+                    href="https://www.linkedin.com/in/gabriel-oliveira-4b58b9224/"
+                    role="button"
+                    target="__blank"
+                    ><i class="fab fa-linkedin-in"></i
+                ></a>
+                <!-- Github -->
+                <a
+                    class="btn text-white btn-floating m-1"
+                    style="background-color: #333333;"
+                    href="https://github.com/GabrielOliveira1996"
+                    role="button"
+                    target="__blank"
+                    ><i class="fab fa-github"></i
+                ></a>
+                </section>
+                <!-- Section: Social media -->
+            </div>
+            <!-- Grid container -->
 
+            <!-- Copyright -->
+            <div class="text-center p-3 bg-primary" style="background-color: rgba(0, 0, 0, 0.2);">
+                © 2022 Copyright:
+                <a class="text-white" href="#">MyFitness</a>
+            </div>
+            <!-- Copyright -->
+            </footer>
+        @endif
 
     </div>
-
-    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-        <div class="col-md-4 d-flex align-items-center">
-            <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
-                <svg class="bi" width="30" height="24">
-                    <use xlink:href="#bootstrap"></use>
-                </svg>
-            </a>
-            <span class="text-muted">© 2023 MyFitness, Inc</span>
-        </div>
-
-        <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
-            <li class="my-5 mx-5"><a class="text-muted text-decoration-none" href="https://github.com/GabrielOliveira1996" target="_blank"><img src="{{ asset('img/icons/github.png') }}" height="32" width="32"> Acesse o meu github =)</a></li>
-        </ul>
-    </footer>
-
 </body>
 
 </html>
