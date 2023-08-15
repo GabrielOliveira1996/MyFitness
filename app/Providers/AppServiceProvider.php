268713ae7ec31c95493db98ef1baf85633308055
Gabriel Oliveira <gab-oliveira96@hotmail.com>
2023-08-15 11:40:42 -0300

    feat: :sparkles: AppServiceProvider
    Implementada uma condição para a utilização de métodos com base em uma variável de ambiente. Essa abordagem é empregada para distinguir entre ambientes de desenvolvimento e produção.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\App;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,5 +21,8 @@ class AppServiceProvider extends ServiceProvider
     {
         Paginator::useBootstrapFive();
         Paginator::useBootstrapFour();
+        if(env('FORCE_HTTPS',false)){
+            URL::forceScheme('https');
+        }
     }
 }
