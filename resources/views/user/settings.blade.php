fd96f226e1a5bcd8c0aa22662e3c62f550491e68
Gabriel <gab-oliveira96@hotmail.com>
2023-09-10 14:54:50 -0300

    style: :art: Design de página settings
    Design de página foi modificado. Foto de perfil fica lado a lado com input 'name' e 'bio', formando duas colunas.

--- resources/views/user/settings.blade.php
+++ resources/views/user/settings.blade.php
@@ -4,55 +4,61 @@
 
 <div class="container">
 
-    <div class="row justify-content-center mt-2">
+    <div class="row mt-2">
 
         <h3 class="text-center mt-5"><strong>{{ __('messages.publicProfile') }}</strong></h3>
 
         <hr/>
 
-        <p class="text-center">{{ __('messages.publicProfileDescription') }}</p>
+        <label class="text-center mb-5">{{ __('messages.publicProfileDescription') }}</label>
 
         <form method="POST" 
                 action="{{ route('settings.edit') }}"
                 autocomplete="off"
                 enctype="multipart/form-data">
             @csrf
-
-            <div class="row mb-3 d-flex justify-content-center">
-                <div class="col-md-3">
+            
+            <div class="row mb-3">
+                <div class="col-md-4">
                     <label for="imageInputId" 
                             class="d-flex justify-content-center">
                         <img id="imageId"
                                 src="{{ $user['profile_image'] ? asset('img/' . $user['profile_image']) : asset('img/user-image.png') }}" 
-                                class="img rounded-circle img-fluid profile-image col-md-12">
+                                class="col-md-12 profile-image">
                     </label>    
                 </div>
-            </div>
-            <input id="imageInputId" 
+
+                <input id="imageInputId" 
                     type="file" 
                     name="profile_image"
                     class="d-none">
 
-            <div class="row mb-3  d-flex justify-content-center">
-                <div class="col-md-6 inputBox">
-                    <input id="name" type="text" name="name" value="{{ $user->name }}" required autofocus>
-                    <label for="name" class="labelInput">{{ __('messages.Name') }}</label>
+                <div class="col-md-6">
+                    <div class="row">
+                        <div class="col-md-12 inputBox">
+                            <input id="name" type="text" name="name" value="{{ $user->name }}" required autofocus>
+                            <label for="name" class="labelInput">{{ __('messages.Name') }}</label>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-md-12 inputBox align-self-center">
+                            <label class="" for="bio">Aqui fica a sua bio. Escreva algo sobre você...</label>
+                            <textarea name="bio" class="resize-false mt-1" cols="10" rows="10" draggable="false">{{ $user->bio }}</textarea>
+                        </div>
+                    </div>
                 </div>
+
+            </div>
+
+            <div class="row">
                 <div class="col-md-12">
-                    @error('name')
+                    @error('bio')
                         <small class="text-danger" role="alert">
                             <strong>{{ $message }}</strong>
                         </small>
                     @enderror
-                </div>
-            </div>
-            <div class="row mb-3 d-flex justify-content-center">
-                <div class="col-md-6 inputBox">
-                    <label class="d-flex justify-content-center" for="bio">Aqui fica a sua bio. Escreva algo sobre você...</label>
-                    <textarea name="bio" class="resize-false mt-1" cols="30" rows="10" draggable="false">{{ $user->bio }}</textarea>
-                </div>
-                <div class="col-md-12">
-                    @error('bio')
+                    @error('name')
                         <small class="text-danger" role="alert">
                             <strong>{{ $message }}</strong>
                         </small>
