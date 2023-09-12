c2d5b845fa4f7f23c87d965b56d9d1818c18ce48
Gabriel <gab-oliveira96@hotmail.com>
2023-09-12 10:23:39 -0300

    refactor: :recycle: Alterações na view 'settings'
    Anteriormente existia apenas um formulário para atualizar informações públicas do perfil e foto. Agora existem dois formulários: um para atualizar a imagem de perfil, e outro para atualizar apenas as informações públicas do perfil.

--- resources/views/user/settings.blade.php
+++ resources/views/user/settings.blade.php
@@ -13,26 +13,20 @@
         <label class="text-center mb-5">{{ __('messages.publicProfileDescription') }}</label>
         
         <div id="settings">
-            
-            <!-- Button trigger modal -->
-            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                Launch demo modal
-            </button>
-            <form method="POST" 
-                    action="{{ route('settings.edit') }}"
-                    autocomplete="off"
-                    enctype="multipart/form-data">
-                @csrf
+            <div class="row mb-3">
+                <form method="POST" 
+                        action="{{ route('perfil-image.update') }}"
+                        autocomplete="off"
+                        enctype="multipart/form-data"
+                        class="col-md-4">
+                    @csrf
 
-                <div class="row mb-3">
-                    <div class="col-md-4">
-                        <label for="imageInputId" 
-                                class="d-flex justify-content-center">
-                            <img id="imageId" 
-                                    src="{{ $user['profile_image'] ? asset('img/' . $user['profile_image']) : asset('img/user-image.png') }}" 
-                                    class="col-md-12 profile-image">
-                        </label>    
-                    </div>
+                    <label for="imageInputId" 
+                            class="d-flex justify-content-center">
+                        <img id="imageId" 
+                                src="{{ $user['profile_image'] ? asset('img/' . $user['profile_image']) : asset('img/user-image.png') }}" 
+                                class="col-md-12 profile-image">
+                    </label>   
 
                     <input id="imageInputId" 
                             type="file" 
@@ -59,59 +53,63 @@
                                     </div>
                                 </div>
                                 <div class="modal-footer">
-                                    <button type="button" class="btn btn-primary col-md-12">Salvar</button>
+                                    <button type="submit" class="btn btn-primary col-md-12">Salvar</button>
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    </div>  
+                </form>
 
-                    <div class="col-md-6">
-                        <div class="row">
-                            <div class="col-md-12 inputBox">
-                                <input id="name" type="text" name="name" value="{{ $user->name }}" required autofocus>
-                                <label for="name" class="labelInput">{{ __('messages.Name') }}</label>
-                            </div>
-                        </div>
+                <form method="POST" 
+                        action="{{ route('public-perfil.update') }}"
+                        autocomplete="off"
+                        class="col-md-6">
+                        @csrf
 
-                        <div class="row">
-                            <div class="col-md-12 inputBox align-self-center">
-                                <label class="" for="bio">Aqui fica a sua bio. Escreva algo sobre você...</label>
-                                <textarea name="bio" class="resize-false mt-1" cols="10" rows="10" draggable="false">{{ $user->bio }}</textarea>
-                            </div>
+                    <div class="row">
+                        <div class="col-md-12 inputBox">
+                            <input id="name" type="text" name="name" value="{{ $user->name }}" required autofocus>
+                            <label for="name" class="labelInput">{{ __('messages.Name') }}</label>
                         </div>
                     </div>
 
-                </div>
-
-                <div class="row">
-                    <div class="col-md-12">
-                        @error('bio')
-                            <small class="text-danger" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </small>
-                        @enderror
-                        @error('name')
-                            <small class="text-danger" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </small>
-                        @enderror
+                    <div class="row">
+                        <div class="col-md-12 inputBox align-self-center">
+                            <label class="" for="bio">Aqui fica a sua bio. Escreva algo sobre você...</label>
+                            <textarea name="bio" class="resize-false mt-1" cols="10" rows="10" draggable="false">{{ $user->bio }}</textarea>
+                        </div>
                     </div>
-                </div>
-                
-                @error('profile_image')
-                <div class="d-flex justify-content-center">
-                    <small class="text-danger">
-                        <strong>{{ $message }}</strong>
-                    </small>
-                </div>
-                @enderror
 
-                <div class="row p-2 d-flex justify-content-center">
-                    <button type="submit" 
-                            class="btn btn-primary col-md-6">{{__('messages.updateProfile')}}</button>
-                </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            @error('bio')
+                                <small class="text-danger" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </small>
+                            @enderror
+                            @error('name')
+                                <small class="text-danger" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </small>
+                            @enderror
+                        </div>
+                    </div>
+                    
+                    @error('profile_image')
+                    <div class="d-flex justify-content-center">
+                        <small class="text-danger">
+                            <strong>{{ $message }}</strong>
+                        </small>
+                    </div>
+                    @enderror
 
-            </form>
+                    <div class="row p-2 d-flex justify-content-center">
+                        <button type="submit" 
+                                class="btn btn-primary col-md-12">{{__('messages.updateProfile')}}</button>
+                    </div>
+
+                </form>
+            </div>
         </div>
      
         <hr/>
