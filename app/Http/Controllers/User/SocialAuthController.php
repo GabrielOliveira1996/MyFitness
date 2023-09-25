43e9e98a6d5698684d0817a5ccdc965d03021209
Gabriel <gab-oliveira96@hotmail.com>
2023-09-25 11:30:21 -0300

    refactor: :recycle: Função 'googleCallback'
    Foi incluída uma exceção para lidar com erros quando o endereço de e-mail está presente no banco de dados, mas não corresponde a uma conta do Gmail.

--- app/Http/Controllers/User/SocialAuthController.php
+++ app/Http/Controllers/User/SocialAuthController.php
@@ -2,59 +2,51 @@
 
 namespace App\Http\Controllers\User;
 
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Repository\User\IUserRepository;
-use App\Services\UserService;
 use Laravel\Socialite\Facades\Socialite;
 use Illuminate\Support\Facades\Auth;
-use Laravel\Socialite\Two\InvalidStateException;
 
 class SocialAuthController extends Controller
 {
-    private $_request;
-    private $_userService;
     private $_socialite;
     private $_userRepository;
 
-    public function __construct(
-        Request $request,
-        UserService $userService,
-        Socialite $socialite,
-        IUserRepository $userRepository
-    ) {
-        $this->_request = $request;
+    public function __construct(Socialite $socialite, IUserRepository $userRepository){
         $this->_socialite = $socialite;
-        $this->_userService = $userService;
         $this->_userRepository = $userRepository;
     }
 
-    public function redirectToGoogle()
-    {
+    public function redirectToGoogle(){
         return Socialite::driver('google')->redirect();
     }
 
-    public function googleCallback()
-    {
-        $user = Socialite::driver('google')->user(); 
-        $email = $user['email'];
-        $findGoogleUser = $this->_userRepository->findUserByEmail($email);
-        if ($findGoogleUser == null) {  // Se email não existir. Criado novo usuário com google_id e entra.
-            $user = [
-                'name' => $user['given_name'],
-                'email' => $user['email'],
-                'password' => $user['id'],
-                'google_id' => $user['id']
-            ];
-            $newUser = $this->_userRepository->createGoogleUser($user);
-            Auth::login($newUser);
-            return redirect()->route('welcome');
+    public function googleCallback(){
+        try{
+            $user = Socialite::driver('google')->user(); 
+            $email = $user['email'];
+            $findGoogleUser = $this->_userRepository->findUserByEmail($email);
+            // Se email não existir. Criado novo usuário com google_id e entra.
+            if ($findGoogleUser == null) {  
+                $user = [
+                    'name' => $user['given_name'],
+                    'email' => $user['email'],
+                    'password' => $user['id'],
+                    'google_id' => $user['id']
+                ];
+                $newUser = $this->_userRepository->createGoogleUser($user);
+                $auth = Auth::login($newUser);
+                return redirect()->route('welcome');
+            }
+            // Se email existir com google_id ele entra.
+            if($findGoogleUser['google_id']){ 
+                $auth = Auth::login($findGoogleUser);
+                return redirect()->route('welcome');
+            }
+            // Se email existir, e não for google_id.
+            throw new \Exception('O e-mail já está cadatrado no sistema.', 422);
+        }catch(\Exception $e){
+            return redirect()->route('login')->withErrors(['email' => $e->getMessage()]);
         }
-        if($findGoogleUser['google_id']){ // Se email existir com google_id ele entra.
-            Auth::login($findGoogleUser);
-            return redirect()->route('welcome');
-        }
-        // Se email existir, e não for google_id.
-        return redirect()->route('login')->withErrors(['email' => 'O e-mail já está cadatrado no sistema.']);
     }
 }
