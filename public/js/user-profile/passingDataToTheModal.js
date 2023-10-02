03600a64840a868846d0dfc2743df228d212c8ea
Gabriel <gab-oliveira96@hotmail.com>
2023-10-02 14:16:43 -0300

    feat: :sparkles: Passando dados para model
    Os parâmetros 'id' e 'text' são passados para o modelo a fim de realizar o processo de edição do post.

---
+++ public/js/user-profile/passingDataToTheModal.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $('.edit-post-btn').click(function() {
+        var postId = $(this).data('post-id');
+        var postText = $(this).data('post-text');
+
+        // Define o ID do post no hidden input.
+        $('#post_id').val(postId);
+
+        // Preenche o campo de texto do post no textarea do modal.
+        $('#updatePostModal').find('textarea[name="text"]').val(postText);
+    });
+});
