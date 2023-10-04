85af9736791a169302a82292cd6569f4dee13047
Gabriel <gab-oliveira96@hotmail.com>
2023-10-04 12:01:05 -0300

    feat: :sparkles: Iniciado desenvolvimento de comentários
    Criado o model e miration para os comentários. A entidade 'Comment' possui os parâmetros 'id', 'user_id' (type: foreign), 'post_id' (type: foreign), 'comment_id' (type: foreign) e 'text' (type: string). Nessa entidade existem os relacionamentos com as entidades 'User' e 'Post' sendo elas belongsTo.

---
+++ app/Models/Comment.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use App\Models\Post;
+
+class Comment extends Model
+{
+
+    protected $fillable = [
+            'user_id',
+            'post_id',
+            'comment_id',
+            'text'
+    ];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function post(){
+        return $this->belongsTo(Post::class);
+    }
+}
